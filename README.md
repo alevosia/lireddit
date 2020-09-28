aaa8c41362d0d1cd91e5dc9a7b44559de57476b4
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2020-09-28 18:15:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lireddit 
+
+A fullstack tutorial project by [Ben Awad](https://www.youtube.com/watch?v=I6ypD7qv3Z8).
+
+## Technologies Used:
+- React
+- TypeScript
+- GraphQL
+- URQL/Apollo
+- Node.js
+- PostgreSQL
+- MikroORM/TypeORM
+- Redis
+- Next.js
+- TypeGraphQL
+- Chakra
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+import { Post } from './entities/Post'
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !__prod__,
+    })
+}
