19c7e22305fc58ada2338fb45efdc57be1a2a334
Alexander Paul Marinas <alexanderpaul.marinas@gmail.com>
2020-09-29 20:04:55 +0800

    Setup express and apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,43 @@
+import 'reflect-metadata'
+
 import { MikroORM } from '@mikro-orm/core'
 import { __prod__ } from './constants'
 import { Post } from './entities/Post'
+import mikroConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
+import { UserResolver } from './resolvers/user'
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: [Post],
-        dbName: 'lireddit',
-        type: 'postgresql',
-        debug: !__prod__,
+    const orm = await MikroORM.init(mikroConfig)
+    await orm.getMigrator().up() // run migrations
+
+    const posts = await orm.em.find(Post, {})
+
+    console.log(posts)
+
+    const app = express()
+
+    app.get('/', (_, res) => {
+        res.send('Hello World!')
+    })
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver, UserResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+    })
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log('Server listening to port 4000.')
     })
 }
+
+main().catch(console.error)
